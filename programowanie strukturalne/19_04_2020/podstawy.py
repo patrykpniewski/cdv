98c2d9eda2a0e44cf9ffdca2fd1c9cee8587b999
Patryk Pniewski <bezenno@gmail.com>
2020-04-19 18:40:56 +0200

    Podstawy

---
+++ programowanie strukturalne/19_04_2020/podstawy.py
@@ -0,0 +1,31 @@
+# fullName = input("Podaj imię i nazwisko: ")
+# age = input("Podaj swój wiek: ")
+#
+#
+# print(f"Imie i nazwisko: {fullName}, wiek: {age}")
+
+"""
+Użytkownik podaje z klawiatury
+imiona i nazwiska oraz wiek
+wyświetl w formacie:
+Imie i nazwisko: ..., wiek: ...
+"""
+
+x = "5"
+print(type(x))
+x = int(x)
+print(type(x))
+
+y = 4
+print(type(y))
+y /= 2
+print(type(y))
+print(y)
+
+surname = "Kowalski"
+print(surname[0])       # K
+print(surname[0:3])     # Kow
+print(surname[-2])      # k
+print(surname[-2:])     # ki
+print(surname[:-2])     # Kowals
+print(surname[:-2:2])   # Kwl
